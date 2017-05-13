fb00fb3c669c3d83f1b72fc90d78c8280707197d
szg2008 <l>
2017-05-13 16:38:06 +0800

    react

--- src/components/GalleryApp.js
+++ src/components/GalleryApp.js
@@ -1,23 +1,23 @@
 'use strict';
 
 var React = require('react/addons');
-var ReactTransitionGroup = React.addons.TransitionGroup;
-
 // CSS
 require('normalize.css');
 require('../styles/main.css');
-
-var imageURL = require('../images/yeoman.png');
-
+//获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
+//利用自执行函数，将图片名信息转成图片URL路径信息
+imageDatas = (function getImageURL(imageDataArr){
+	for(var i = 0; i < imageDataArr.length; i++){
+		var singleImageData = imageDataArr[i];
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+		imageDataArr[i] = singleImageData;
+	}
+	return imageDataArr;
+})(imageDatas);
 var GalleryApp = React.createClass({
   render: function() {
-    return (
-      <div className="main">
-        <ReactTransitionGroup transitionName="fade">
-          <img src={imageURL} />
-        </ReactTransitionGroup>
-      </div>
-    );
+    return (<section className="stage"><section className="img-sec"></section><nav className="controller-nav"></nav></section>);
   }
 });
 React.render(<GalleryApp />, document.getElementById('content')); // jshint ignore:line
