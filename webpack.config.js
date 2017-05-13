1684c2a51db93b2228a465e2d34f05e5e145bc40
szg2008 <l>
2017-05-13 18:31:01 +0800

    react

--- webpack.config.js
+++ webpack.config.js
@@ -36,11 +36,11 @@ module.exports = {
     }
   },
   module: {
-    preLoaders: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: 'eslint-loader'
-    }],
+    // preLoaders: [{
+    //   test: /\.(js|jsx)$/,
+    //   exclude: /node_modules/,
+    //   loader: 'eslint-loader'
+    // }],
     loaders: [{
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
